e415799dcf3fa7f98eb76eec8d0b9e8e2e6611ed
Rutuja2024 <rutujahujare20@gmail.com>
2023-12-03 17:08:43 +0530

    final commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const middlewares = jsonServer.defaults()
  
 server.use(middlewares)
 server.use('/api', router)
-server.listen(process.env.PORT || 5000, () => {
+server.listen(process.env.PORT || 8000, () => {
   console.log('JSON Server is running')
 })
 
